7dd13a4103443f14bf92ae28611b42983872f7d7
Layth <Layth@DESKTOP-L1OEB95>
2017-02-08 22:16:55 +0100

    Add project files.

---
+++ Foursquare/Foursquare/Views/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Foursquare.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Foursquare 
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public async void click (object Sender, EventArgs e)
+        {
+            public var text = MainEntry.Text;
+           await Navigation.PushModalAsync(new Resultat(text));
+        }
+    }
+}
